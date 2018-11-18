ed445288e626cf7555dc707e5947e7acc7a56fd4
Kim Hendrick <kim@earthvectors.com>
2018-11-18 11:36:03 -0700

    End of video 12

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,19 @@
-// let's go!
+require('dotenv').config({path: 'variables.env'});
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+// todo: use express middleware to handle cookies (JWT)
+// todo: use express middleware to popuplate current user
+
+server.start({
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+    },
+  },
+  deets => {
+    console.log(`Server is running on http://localhost:${deets.port}`);
+  }
+);
